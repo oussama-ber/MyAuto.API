bcafb1b60ef557d2bcd27a48271e1a001a571821
oussema <oussema.benrejab@gmail.com>
2023-08-31 19:49:25 +0100

    start working on save offre voiture

--- models/TestImage.js
+++ models/TestImage.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const testImageSchema = mongoose.Schema({
     voitureId: { type : mongoose.Schema.Types.ObjectId, ref: "Voiture", required : false},
-    fileBase64: { type: String}
+    fileBase64: { type: String},
+    fileURL: { type: String}
 });
 
 module.exports = mongoose.model('TestImage', testImageSchema);
